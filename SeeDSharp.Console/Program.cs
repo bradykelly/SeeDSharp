62041611a87fcb8fefc8b401d8e160bd2ab6e93a
Brady Kelly <brady@bradykelly.net>
2020-10-26 08:45:34 +0200

    First command working.

---
+++ SeeDSharp.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+
+namespace SeeDSharp.Console
+{
+    class Program
+    {
+        private static DiscordClient discord;
+        private static CommandsNextModule commands;
+        private static InteractivityModule interactivity;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = await File.ReadAllTextAsync(@"..\..\..\..\secrets\token"),
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = ";;"
+            });
+            commands.RegisterCommands<SimpleCommands>();
+
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration());
+
+            await discord.ConnectAsync();
+            System.Console.WriteLine("Connected to Discord");
+
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ SeeDSharp.Console/SimpleCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+
+namespace SeeDSharp.Console
+{
+    public class SimpleCommands
+    {
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Author.Id == ctx.User.Id 
+                                                                    && xm.Content.ToLower() == "how are you?", TimeSpan.FromMinutes(1));
+            if (msg != null)
+                await ctx.RespondAsync($"I'm fine, thank you!");
+
+        }
+    }
+}
